460c708a0024cdee3ae49c7dd2494712a60c2065
Daria <dashulya-pustohailova@mail.ru>
2020-09-27 19:38:27 +0300

    last version

--- OpenCV.cpp
+++ OpenCV.cpp
@@ -4,18 +4,19 @@ using namespace cv;
 int main(int argc, char** argv) {
 
     Mat image1 = imread("D:\main.jpg");
-    Mat image2 = imread("D:\own_12.jpg");
+    Mat image2 = imread("D:\own_12+2.jpg");
     //Mat image2 = imread("D:\ore.jpg");
     Mat outImg1;
     Mat outImg2;
     Mat res;
-    resize(image1, outImg1, cv::Size(), 0.4, 0.4);
-    resize(image2, outImg2, cv::Size(), 0.4, 0.4);
+    resize(image1, outImg1, Size(), 0.4, 0.4);
+    resize(image2, outImg2, Size(), 0.4, 0.4);
     Mat input = outImg1;
     Mat templ = outImg2;
-    Mat gray;
+    //Mat gray;
     double minVal; double maxVal; Point minLoc; Point maxLoc;
     Point matchLoc;
+    int match_method = TM_CCORR_NORMED;// TM_CCORR;
     //cvtColor(input, gray, CV_BGR2GRAY);
 
     Mat result;
@@ -24,8 +25,6 @@ int main(int argc, char** argv) {
     int result_rows = input.rows - templ.rows + 1;
 
     result.create(result_cols, result_rows, CV_32FC1);
-
-    int match_method = TM_CCORR_NORMED;// TM_CCORR;
 
     // Do the Matching and Normalize
     matchTemplate(input, templ, result, match_method);
@@ -51,7 +50,6 @@ int main(int argc, char** argv) {
     imshow("input", input);
     imshow("template", templ);
 
-    imwrite("../outputData/TemplateMatch.jpg", input);
     waitKey(0);
 
     return 0;
